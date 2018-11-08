a06bfd50870e693a968bd724c0dcf45615d44875
mellem <cph-ml474@cphbusiness.dk>
2018-11-08 17:53:31 +0100

    indentdation formatting

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 
 /**
  *  view engine setup
- */ 
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -36,14 +36,14 @@ app.use('/api', apiRouteManager);
 /**
  *  catch 404 and forward to error handler
  */
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 /**
  *  error handler
  */
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

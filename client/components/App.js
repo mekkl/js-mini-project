30e242a483502162e024169800ac23e1a359765d
mekkl <mikkel7emil@gmail.com>
2018-12-22 13:08:30 +0100

    add react routing

---
+++ client/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import StartPage from './StartPage';
+import InfoPage from './InfoPage';
+
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router basename='/node/react'>
+                <div className="app">
+                    <div className='sideMenu'>
+                    <ul>
+                        <li><Link to='/'>start page</Link></li>
+                        <li><Link to='/info'>info page</Link></li>
+                    </ul>                  
+                    </div>
+                    <Switch>
+                        <Route exact path="/" component={StartPage} />
+                        <Route exact path="/info" component={InfoPage} />
+                    </Switch>
+                </div >
+            </Router >
+        );
+    }
+}
---
+++ client/components/InfoPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class InfoPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div >
+                <h1>info page</h1>
+            </div>
+        )
+    }
+}
